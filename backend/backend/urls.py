568a80daa89d3677d5898ce857321f34264a137d
Harshith-2009 <manupatiharshith@gmail.com>
2025-02-07 20:48:56 +0530

    h

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('inventory/', InventoryPage.as_view(), name='inventory')
 ]
 
-"harshith's urls signup
+"harshith's urls signup"
 """
 URL configuration for src project.
 
@@ -47,5 +47,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('first.urls')),  # Include the first app's URLs
+    path('api/', include('first.urls')),  # Include the first app's URL s 
 ]
