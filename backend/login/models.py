3b2735217f2233a99c85929fa325de64801eb46a
Kushagra-o7 <kushagrakushwaha07@gmail.com>
2025-02-07 19:11:57 +0530

    table for users mapped with inventory

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    name = models.CharField(max_length=50)
+    code=models.CharField(max_length=3)
+    quantity=models.IntegerField()
+    owner = models.ForeignKey(User)
