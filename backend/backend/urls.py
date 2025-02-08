757ccde9a10fc4c27382a8a70b642a8321facd9a
Harshith-2009 <manupatiharshith@gmail.com>
2025-02-08 14:58:21 +0530

    adding all urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,12 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from login.views import LoginApi
 from login.views import InventoryPage
+from login.views import NewProduct
+from login.views import ProductDetail
+from login.views import LogoutApi
+
 
 urlpatterns = [
-    path('login/', LoginApi.as_view(), name='login'),
     path('admin/', admin.site.urls),
-    path('api/', include('login.urls')),
+
+    
+    path('login/', LoginApi.as_view(), name='login'),
+    path('logout/', LogoutApi.as_view(), name='logout'),
+
+    
     path('inventory/', InventoryPage.as_view(), name='inventory'),
+    path('inventory/new/', NewProduct.as_view(), name='new_product'),
+    path('inventory/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+
+    
+    path('api/', include('login.urls')),
 ]
 
 
